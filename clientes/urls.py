3ba5fa19e5cf739a9fd6e15e7656041729053889
VictorKiill <30813666+VictorKiill@users.noreply.github.com>
2020-03-08 23:02:20 +0000

    Update urls.py

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/', listar_clientes, name='listar_clientes'),
+    path('listar', listar_clientes, name='listar_clientes'),
     path('cadastrar', inserir_cliente, name='cadastrar_cliente'),
     path('listar/<int:id>', listar_cliente_id, name='listar_cliente_id'),
     path('editar/<int:id>', editar_cliente, name='editar_cliente'),

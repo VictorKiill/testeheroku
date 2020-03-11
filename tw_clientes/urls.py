616499c0f38e85d9473b606548b4d79bb24858a7
VictorKiill <30813666+VictorKiill@users.noreply.github.com>
2020-03-11 20:10:19 -0300

    Update urls.py

--- tw_clientes/urls.py
+++ tw_clientes/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from .views import logout_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
     path('', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout')
 ]

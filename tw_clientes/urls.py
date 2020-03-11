7d6f4cfc5dbd6c5690b21e877d5f821210f44fba
VictorKiill <30813666+VictorKiill@users.noreply.github.com>
2020-03-11 19:55:09 -0300

    Update urls.py

--- tw_clientes/urls.py
+++ tw_clientes/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
     path('', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
